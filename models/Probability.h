e354d50f87204409e45f522de744ec340d98a8d2
paul-autopv <paul@pm.me>
2022-01-01 17:22:25 +0200

    Added Porbability Interface

---
+++ models/Probability.h
@@ -0,0 +1,12 @@
+//
+// Created by Paul on 2022/01/01.
+//
+
+#ifndef TREE_PROBABILITY_H
+#define TREE_PROBABILITY_H
+
+class Probability{
+    virtual bool hasOccurred() = 0;
+};
+
+#endif //TREE_PROBABILITY_H
