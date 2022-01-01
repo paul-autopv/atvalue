43a58b1f56da55a4e48ab0fcbe69c13a10f04786
paul-autopv <paul@pm.me>
2022-01-01 18:08:40 +0200

    Added csvreader for failure modes and facility

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 
 #include "models/Unit.h"
 #include "models/Facility.h"
+#include "utilities/CsvReader.h"
 
 int main() {
     std::cout << "Hello, AtValue!" << std::endl;
 
+    CsvReader reader = CsvReader();
+    auto station = reader.readCsv("/Users/paul/Repos/tree/data/model_1.csv", true);
+    auto failure_modes = reader.readCsv("/Users/paul/Repos/tree/data/failure_modes.csv", true);
+
     auto facility = make_unique<Facility>();
+
     try {
         facility->addRoot(new Unit(0, "Root"));
         facility->addUnit(new Unit(1, "Unit 1"), 0);
