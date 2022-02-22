01a19886b702105c91a537ea0a38c86ca448b8ff
Paul Nel <Paul.Nel@zutari.com>
2022-02-22 13:01:48 +0200

    AV-26 #comment Implemented detail for Outage class

--- models/Outage.h
+++ models/Outage.h
@@ -6,10 +6,60 @@
 #define ATVALUE_OUTAGE_H
 
 
+#include "OutageSchedule.h"
+#include "OutageCost.h"
+#include "OutageType.h"
+
 class Outage {
 
-    int startDay {0};
-    int
+public:
+    Outage() = delete;
+    Outage(int id, int unit_id, OutageType type, OutageSchedule schedule, OutageCost cost):
+        id_ {id},
+        unit_id_ {unit_id},
+        type_ {type},
+        schedule_ {schedule},
+        cost_ {cost}{}
+
+    int start() const{
+        return schedule_.start();
+    }
+
+    int end() const{
+        return schedule_.end();
+    }
+
+    int duration() const{
+        return schedule_.duration();
+    }
+
+    double capex_cost() const{
+        return cost_.capex();
+    }
+
+    double opex_cost() const{
+        return cost_.opex();
+    }
+
+    double total_cost() const{
+        return cost_.total();
+    }
+
+    double daily_cost() const{
+        return total_cost() / duration();
+    }
+
+    OutageType type() const{
+        return type_;
+    }
+
+private:
+    int id_;
+    int unit_id_;
+    OutageType type_;
+    OutageSchedule schedule_;
+    OutageCost cost_;
+
 };
 
 
